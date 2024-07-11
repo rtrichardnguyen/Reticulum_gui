882265b2493db46697759ed15cb12850ab9d0e89
rtricharnguyen <rtrichardnguyen@gmail.com>
2024-07-11 13:33:55 -0400

    worker thread polling

--- worker.py
+++ worker.py
@@ -3,8 +3,22 @@ import subprocess
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import QThread, pyqtSignal
 
+
 class Worker(QThread):
     output_signal = pyqtSignal(str)
 
     def run(self):
-        pass
+        process = subprocess.Popen(
+            ['nomadnet'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        while True:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                self.output_signal.emit(output.strip())
+
+            remaining = process.communicate()[0]
+            
+            if remaining:
+                self.output_signal.emit(remaining.strip())
